0f85d5f4584afe050043cab0f80295f86e19c643
Enzo Gabriel R B Moreira <egabrielrbm@gmail.com>
2023-09-19 08:55:24 -0300

    Algoritmo quicksort em C

---
+++ main.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <locale.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] >= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+    int arr[7];
+    int i;
+
+    printf("Digite 7 números:\n");
+
+    for (i = 0; i < 7; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    quickSort(arr, 0, 6);
+
+    printf("Array ordenado do maior para o menor:\n");
+
+    for (i = 0; i < 7; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
